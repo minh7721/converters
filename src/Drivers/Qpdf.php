9d15a20df83f32df4e5e4b8b2112643f616c7764
Nguyễn Văn Hiệp <Nguyenhiepvan.bka@gmail.com>
2021-04-05 11:57:30 +0700

    update qpdf

--- src/Drivers/Qpdf.php
+++ src/Drivers/Qpdf.php
@@ -14,18 +14,18 @@ use Colombo\Converters\Exceptions\ConvertException;
 use Colombo\Converters\Process\CanRunCommand;
 
 class Qpdf extends CanRunCommand implements ConverterInterface {
-    
+
     protected $bin = 'qpdf';
-    
+
     protected $process_options = [
         '--linearize' => true,
         '--min-version=1.5' => true,
-        '--object-streams' => "generate",
+        '--object-streams=generate' => true,
     ];
-    
+
     protected $start_page = 0;
     protected $end_page = 0;
-    
+
     /**
      * qpdf --linearize --collate --empty --min-version=1.5 --pages qpdf.pdf 1-10 -- qpdf.o.pdf
      * @param $path
@@ -36,8 +36,8 @@ class Qpdf extends CanRunCommand implements ConverterInterface {
      */
     public function convert( $path, $outputFormat, $inputFormat = '' ): ConvertedResult {
         $result = new ConvertedResult();
-    
-        $command = $this->buildCommand($path);
+
+        $command = $this->buildCommand([$path]);
         try{
             $this->run( $command);
             $result->setContent( $this->output() );
@@ -46,25 +46,33 @@ class Qpdf extends CanRunCommand implements ConverterInterface {
         }
         return $result;
     }
-    
-    protected function buildCommand($path = ''){
-        $command = $this->bin . $this->buildOptions(true);
-        
+
+    protected function buildCommand(array $append = [], array $prepend = [], $use_equal_symbol = false) : array{
+        $command = parent::buildCommand([], $prepend);
+
         if($this->start_page || $this->end_page){
             $this->start_page = $this->start_page ? $this->start_page : 1;
             $this->end_page = $this->end_page ? $this->end_page : 'z';
-            $command .= " --collate --empty --pages " . $path . " " . $this->start_page . "-" . $this->end_page . " -- -";
+            $command[] = '--collate';
+            $command[] = '--empty';
+            $command[] = '--pages';
+            $command[] = $append[0];
+            $command[] = $this->start_page . "-" . $this->end_page;
+            $command[] = "--";
+            $command[] = "-";
         }else{
-            $command .= " " . $path . " -";
+            $command = array_merge($command, $append, ["-"]);
         }
-        
+
+//        dd($command);
+
         return $command;
     }
-    
+
     public function startPage( int $page ) {
         $this->start_page = $page;
     }
-    
+
     public function endPage( int $page ) {
         $this->end_page = $page;
     }
