b60025cd9a17b4dc233ee4ee140e0232b6a2acf5
Nguyễn Văn Hiệp <nguyenhiepvan.bka@gmail.com>
2021-08-18 16:00:31 +0700

    update pdf2xml

--- src/Drivers/PdfToXml.php
+++ src/Drivers/PdfToXml.php
@@ -9,14 +9,16 @@
 namespace Colombo\Converters\Drivers;
 
 
-class PdfToXml extends PdfToHtml {
-	
-	protected $process_options = [
-		'-i' => true,
-		'-xml' => true,
-		'-stdout' => true,
-		'-nodrm' => true,
-		'-fontfullname' => true,
-	];
-	
+class PdfToXml extends PdfToHtml
+{
+
+    protected $process_options = [
+        '-i'            => true,
+        '-xml'          => true,
+        '-stdout'       => true,
+        '-hidden'       => true,
+        '-nodrm'        => true,
+        '-fontfullname' => true,
+    ];
+
 }
